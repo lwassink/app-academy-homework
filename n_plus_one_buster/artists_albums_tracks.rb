0dd1ecb54c3fb10526090459b43fdf37d449e689
Luke Wassink <lwassink@gmail.com>
2016-10-02 18:56:34 -0700

    finish week three

---
+++ n_plus_one_buster/artists_albums_tracks.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/ruby
+#
+# Copyright (C) 2016 Luke Wassink <lwassink@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# app/models/track.rb
+class Track
+   belongs_to(
+    :album,
+    class_name: "Album",
+    foreign_key: :album_id,
+    primary_key: :id
+  )
+end
+
+
+# app/models/album.rb
+class Album
+  belongs_to(
+    :artist,
+    class_name: "Artist",
+    foreign_key: :artist_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :tracks,
+    class_name: "Track",
+    foreign_key: :album_id,
+    primary_key: :id
+  )
+end
+
+
+# app/models/artist.rb
+class Artist
+  has_many(
+    :albums,
+    class_name: "Album",
+    foreign_key: :artist_id,
+    primary_key: :id
+  )
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.name] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    albums = self.albums
+    tracks_count = {}
+
+    counts = albums
+      .select("albums.name, COUNT(*) AS track_count")
+      .joins(:tracks)
+      .group("albums.id")
+
+    counts.each do |album|
+      tracks_count[album.name] = album.tracks_count
+    end
+
+    tracks_count
+  end
+end
---
+++ n_plus_one_buster/routes_buses_drivers.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/ruby
+#
+# Copyright (C) 2016 Luke Wassink <lwassink@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# app/models/driver.rb
+class Driver
+  belongs_to(
+    :bus,
+    class_name: "Bus",
+    foreign_key: :bus_id,
+    primary_key: :id
+  )
+end
+
+
+# app/models/bus.rb
+class Bus
+  belongs_to(
+    :route,
+    class_name: "Route",
+    foreign_key: :route_id,
+    primary_key: :id,
+  )
+
+  has_many(
+    :drivers,
+    class_name: "Driver",
+    foreign_key: :bus_id,
+    primary_key: :id,
+  )
+end
+
+
+# app/models/route.rb
+class Route
+  has_many(
+    :buses,
+    class_name: "Bus",
+    foreign_key: :route_id,
+    primary_key: :id
+  )
+
+  def n_plus_one_drivers
+    buses = self.buses
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+
+  def better_drivers_query
+    buses = self.buses
+      .includes(:drivers)
+      .select("buses.id AS bus_id, drivers.name AS driver_name")
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      buses.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+end
+
