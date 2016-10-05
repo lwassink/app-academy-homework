5d7be2dd687998f245680d947b1a9c895e1aa0da
Luke Wassink <lwassink@gmail.com>
2016-10-04 19:04:24 -0700

    update homework to w4d1

--- library/app/controllers/books_controller.rb
+++ library/app/controllers/books_controller.rb
@@ -1,22 +1,23 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
   end
 
-  def new
-    # your code here
-  end
+  # def new
+  # end
 
   def create
-    # your code here
+    Book.create(title: book_params[:title], author: book_params[:author])
+    redirect_to :books
   end
 
   def destroy
-    # your code here
+    Book.find(params[:id]).destroy
+    redirect_to :books
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :author)
+    params.require(:book).permit(:title, :author, :id)
   end
 end
