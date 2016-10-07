eadeba211b8fdb0533754f6250fbf8dd3d2196a4
Luke Wassink <lwassink@gmail.com>
2016-10-06 21:54:44 -0700

    finish w4d4 hw

--- AuthHomework/app/models/user.rb
+++ AuthHomework/app/models/user.rb
@@ -23,13 +23,17 @@ class User < ApplicationRecord
   end
 
   def self.generate_session_token
-    session[:session_token] ||= SecureRandom.urlsafe_base64
-    session[:session_token]
+    SecureRandom.urlsafe_base64
   end
 
   def reset_session_token!
-    session[:session_token] = SecureRandom.urlsafe_base64
+    self.session_token = SecureRandom.urlsafe_base64
     self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
   attr_reader :password
